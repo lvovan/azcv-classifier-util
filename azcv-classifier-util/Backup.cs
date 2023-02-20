d470ff6407e08cf73176528e6758155ecd5d4095
Arnaud Tincelin <25585350+arnaud-tincelin@users.noreply.github.com>
2023-02-20 11:54:32 +0100

    Improved Exception handling

--- azcv-classifier-util/Backup.cs
+++ azcv-classifier-util/Backup.cs
@@ -54,82 +54,75 @@ namespace azcv_classifier_util
       {
         var rootPath = Path.GetFullPath(Path.Join(this.options.Path, this.options.ProjectId.ToString()));
 
-        try
+        if (Directory.Exists(rootPath))
         {
-          if (Directory.Exists(rootPath))
-          {
-            Directory.Delete(rootPath, true);
-          }
-          Directory.CreateDirectory(rootPath);
+          Directory.Delete(rootPath, true);
+        }
+        Directory.CreateDirectory(rootPath);
 
-          var projectMeta = await client.ExportProjectAsync(this.options.ProjectId);
-          var project = await client.GetProjectAsync(this.options.ProjectId);
-          var tags = await client.GetTagsAsync(this.options.ProjectId);
-          var images = new List<Image>();
+        var projectMeta = await client.ExportProjectAsync(this.options.ProjectId);
+        var project = await client.GetProjectAsync(this.options.ProjectId);
+        var tags = await client.GetTagsAsync(this.options.ProjectId);
+        var images = new List<Image>();
 
-          var take = 256; // Take maximum value is 256
-          for (int i = 0; i * take <= projectMeta.ImageCount; i++)
-          {
-            images.AddRange(await client.GetImagesAsync(this.options.ProjectId, take: take, skip: i * take));
-          }
+        var take = 256; // Take maximum value is 256
+        for (int i = 0; i * take <= projectMeta.ImageCount; i++)
+        {
+          images.AddRange(await client.GetImagesAsync(this.options.ProjectId, take: take, skip: i * take));
+        }
 
-          var projectDTO = new ProjectBackup()
+        var projectDTO = new ProjectBackup()
+        {
+          Id = project.Id,
+          Name = project.Name,
+          description = project.Description,
+          Images = images.Select(i => new ImageBackup()
           {
-            Id = project.Id,
-            Name = project.Name,
-            description = project.Description,
-            Images = images.Select(i => new ImageBackup()
+            Id = i.Id,
+            Width = i.Width,
+            Height = i.Height,
+            Regions = i.Regions?.Select(r => new RegionBackup()
             {
-              Id = i.Id,
-              Width = i.Width,
-              Height = i.Height,
-              Regions = i.Regions?.Select(r => new RegionBackup()
+              Height = r.Height,
+              Left = r.Left,
+              TagId = r.TagId,
+              Top = r.Top,
+              Width = r.Width
+            })?.ToList(),
+            Uri = i.OriginalImageUri
+          }).ToList(),
+          Tags = tags,
+        };
+
+        var json = JsonConvert.SerializeObject(projectDTO, Formatting.Indented);
+        System.IO.File.WriteAllText(Path.Join(rootPath, "project.json"), json);
+
+        using (var httpClient = new HttpClient())
+        {
+          Func<string, ImageBackup, Task> downloadFile = async (rootPath, image) =>
+          {
+            using (var s = await httpClient.GetStreamAsync(image.Uri))
+            {
+              string imageType;
+              // We need a seekable stream
+              using (var ms = new MemoryStream())
               {
-                Height = r.Height,
-                Left = r.Left,
-                TagId = r.TagId,
-                Top = r.Top,
-                Width = r.Width
-              })?.ToList(),
-              Uri = i.OriginalImageUri
-            }).ToList(),
-            Tags = tags,
+                await s.CopyToAsync(ms);
+                imageType = ImageTypeHelper.GetKnownFileType(ms).ToString();
+              }
+              var path = Path.Join(rootPath, $"{image.Id}.{imageType.ToLowerInvariant()}");
+              using (var fs = new FileStream(path, FileMode.CreateNew, FileAccess.ReadWrite))
+              {
+                await s.CopyToAsync(fs);
+              }
+              image.Path = path;
+            }
           };
 
-          var json = JsonConvert.SerializeObject(projectDTO, Formatting.Indented);
-          System.IO.File.WriteAllText(Path.Join(rootPath, "project.json"), json);
-
-          using (var httpClient = new HttpClient())
-          {
-            Func<string, ImageBackup, Task> downloadFile = async (rootPath, image) =>
-            {
-              using (var s = await httpClient.GetStreamAsync(image.Uri))
-              {
-                string imageType;
-                // We need a seekable stream
-                using (var ms = new MemoryStream())
-                {
-                  await s.CopyToAsync(ms);
-                  imageType = ImageTypeHelper.GetKnownFileType(ms).ToString();
-                }
-                var path = Path.Join(rootPath, $"{image.Id}.{imageType.ToLowerInvariant()}");
-                using (var fs = new FileStream(path, FileMode.CreateNew, FileAccess.ReadWrite))
-                {
-                  await s.CopyToAsync(fs);
-                }
-                image.Path = path;
-              }
-            };
-
-            await Task.WhenAll(projectDTO.Images.Select(i => downloadFile(rootPath, i)));
-          }
-
-          Console.WriteLine($"The project {project.Name} ({project.Id}) was successfully exported to local folder {rootPath}");
+          await Task.WhenAll(projectDTO.Images.Select(i => downloadFile(rootPath, i)));
         }
-        catch (Exception ex)
-        {
-          Console.WriteLine($"Backup failed: {ex.Message}");
-        }
+
+        Console.WriteLine($"The project {project.Name} ({project.Id}) was successfully exported to local folder {rootPath}");
       }
     }
   }
--- azcv-classifier-util/Program.cs
+++ azcv-classifier-util/Program.cs
@@ -11,25 +11,45 @@ namespace azcv_classifier_util
 
     static async Task Main(string[] args)
     {
-      await Parser.Default.ParseArguments<TagVerb, TrainVerb, PublishVerb, AugmentVerb, TestVerb, ResetVerb, MigrateVerb, BackupVerb>(args)
-        .MapResult(
-          (TagVerb opts) => new CreateTag(opts).ProcessAsync(),
-          (TrainVerb opts) => new Train(opts).ProcessAsync(),
-          (PublishVerb opts) =>
-          {
-            Console.WriteLine("Not implemented");
-            return Task.CompletedTask;
-          },
-          (AugmentVerb opts) => new Augment(opts).ProcessAsync(),
-          (TestVerb opts) =>
-          {
-            Console.WriteLine("Not implemented");
-            return Task.CompletedTask;
-          },
-          (ResetVerb opts) => new Reset(opts).ProcessAsync(),
-          (MigrateVerb opts) => new Migrate(opts).ProcessAsync(),
-          (BackupVerb opts) => new Backup(opts).ProcessAsync(),
-          errs => Task.FromResult(1));
+      try
+      {
+        await Parser.Default.ParseArguments<TagVerb, TrainVerb, PublishVerb, AugmentVerb, TestVerb, ResetVerb, MigrateVerb, BackupVerb>(args)
+          .MapResult(
+            (TagVerb opts) => new CreateTag(opts).ProcessAsync(),
+            (TrainVerb opts) => new Train(opts).ProcessAsync(),
+            (PublishVerb opts) =>
+            {
+              Console.WriteLine("Not implemented");
+              return Task.CompletedTask;
+            },
+            (AugmentVerb opts) => new Augment(opts).ProcessAsync(),
+            (TestVerb opts) =>
+            {
+              Console.WriteLine("Not implemented");
+              return Task.CompletedTask;
+            },
+            (ResetVerb opts) => new Reset(opts).ProcessAsync(),
+            (MigrateVerb opts) => new Migrate(opts).ProcessAsync(),
+            (BackupVerb opts) => new Backup(opts).ProcessAsync(),
+            errs => Task.FromResult(1));
+      }
+      catch (Exception ex)
+      {
+        var tEx = ex as Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models.CustomVisionErrorException;
+        var pEx = ex as Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models.CustomVisionErrorException;
+
+        Console.WriteLine(ex.Message);
+
+        if (tEx != null)
+        {
+          Console.WriteLine(tEx.Body?.Message);
+        }
+
+        if (pEx != null)
+        {
+          Console.WriteLine(pEx.Body?.Message);
+        }
+      }
     }
   }
 }
